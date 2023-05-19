8b5dfcafe34399b010660a8b60be4971183d47e6
Samagra <samagra8889@gmail.com>
2023-05-19 18:34:14 +0530

    Added general Rubikscube and different models of RubiksCube

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-
+#include<bits/stdc++.h>
+using namespace std;
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    cout << "I am Moving" << endl;
     return 0;
 }
